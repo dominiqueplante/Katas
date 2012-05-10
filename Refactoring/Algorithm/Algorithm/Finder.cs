8322a494298d5acc92217717acab3fee894be784
Dominique Plante <dominique.plante@gmail.com>
2012-05-10 07:05:25 -0700

    split Finder into two classes - removed switch

--- Refactoring/Algorithm/Algorithm/Finder.cs
+++ Refactoring/Algorithm/Algorithm/Finder.cs
@@ -10,17 +10,31 @@ namespace Algorithm
         {
             _p = p;
         }
-
+        
         public F Find(FT ft)
+        {
+            var tr = PopulateTR();
+
+            if(tr.Count < 1)
+            {
+                return new F();
+            }
+
+            return AnswerCalculator.CalculateAnswer(ft, tr);
+        }
+
+
+
+        private List<F> PopulateTR()
         {
             var tr = new List<F>();
 
-            for(var i = 0; i < _p.Count - 1; i++)
+            for (var i = 0; i < _p.Count - 1; i++)
             {
-                for(var j = i + 1; j < _p.Count; j++)
+                for (var j = i + 1; j < _p.Count; j++)
                 {
                     var r = new F();
-                    if(_p[i].BirthDate < _p[j].BirthDate)
+                    if (_p[i].BirthDate < _p[j].BirthDate)
                     {
                         r.P1 = _p[i];
                         r.P2 = _p[j];
@@ -34,33 +48,49 @@ namespace Algorithm
                     tr.Add(r);
                 }
             }
+            return tr;
+        }
+    }
 
-            if(tr.Count < 1)
+    public class AnswerCalculator
+    {
+        public static F CalculateAnswer(FT ft, List<F> tr)
+        {
+            if (ft == FT.One)
+                return calculateAnswerForOne(tr);
+            if (ft == FT.Two)
             {
-                return new F();
+                return calculateAnswerForTwo(tr);
             }
+            return null;
+        }
 
+        public static F calculateAnswerForOne(List<F> tr)
+        {
             F answer = tr[0];
-            foreach(var result in tr)
+            foreach (var result in tr)
             {
-                switch(ft)
+                if (result.D < answer.D)
                 {
-                    case FT.One:
-                        if(result.D < answer.D)
-                        {
-                            answer = result;
-                        }
-                        break;
-
-                    case FT.Two:
-                        if(result.D > answer.D)
-                        {
-                            answer = result;
-                        }
-                        break;
+                    answer = result;
                 }
             }
+            return answer;
+        }
 
+        public static F calculateAnswerForTwo(List<F> tr)
+        {
+            F answer = tr[0];
+            foreach (var result in tr)
+            {
+                if (result.D > answer.D)
+                {
+                    answer = result;
+                }
+                //break;
+
+
+            }
             return answer;
         }
     }
